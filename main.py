8e7b0534277049b98c83da3c0e3b0530d0856c93
zackakyo <tcheuyassiisaac@yahoo.com>
2021-09-30 10:54:53 +0200

    alluserconnect

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import simon.py
+
+def AllUsersConnection(): 
+    try: 
+        return MC.connect(host = 'localhost', database='eqlaflix', user = 'root', password = 'g5gqwagr' )
+    except:
+        try: 
+            return MC.connect(host = 'localhost', database='eqlaflix', user = 'root', password = 'python4life' )
+        except:
+            return MC.connect(host = 'localhost', database='eqlaflix', user = 'root', password = 'isaac' )
