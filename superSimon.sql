337381539b02e4021d5921e981fba2a06023f261
Mahsum Kizmaz <mahsumkizmaz@gmail.com>
2021-09-30 11:03:31 +0200

    ajout de la table score

--- superSimon.sql
+++ superSimon.sql
@@ -1,44 +1,38 @@
+
 /* On utilise l'encodage UTF8 */
 charset utf8;
 
-DROP DATABASE IF EXISTS eqlaflix;
-CREATE DATABASE eqlaflix;
+DROP DATABASE IF EXISTS supersimon;
+CREATE DATABASE supersimon;
 
 /* On Change de base de données */
-use eqlaflix;
+use supersimon;
 
 SET FOREIGN_KEY_CHECKS=0;
 
 
 
 
-/* Creation de la Table Genre pour les films. */
+/* Creation de la Table Genre pour les score. */
 
 
-DROP TABLE IF EXITS 'films'; 
+DROP TABLE IF EXISTS 'score'; 
 
-CREATE TABLE Films(
-    IdFilm int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    Titre varchar(255),
-    Genre smallint UNSIGNED,
-    Realisateur varchar(255) not null,
-    DateSortie date NOT NULL,
-    BoxOffice int UNSIGNED NOT NULL,
-    pays varchar(150),
-    -- pays int(11),
-    Deleted boolean default FALSE,
-    -- FOREIGN KEY(pays) REFERENCES Pays(Id_Pays),
-    FOREIGN KEY(Genre) REFERENCES GenreFilm(IdGenre)
-);
+CREATE TABLE score(
+    IdScore int UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
+    pseudo varchar(255) not null,
+    score int not null,
+	datescore datetime
+	);
 
 
-INSERT INTO Films(Titre , Genre, Realisateur, DateSortie, BoxOffice, pays )
-VALUES('Titanic' , 14, 'James Cameron', '1997-01-01' , 20634793, 'americain' ), 
-("Bienvenue chez les Ch'tis" , 14, 'Dany Boon', '2008-02-20' , 20489303, "francais" ),
-("Intouchables" , null, "E. Toledano et O. Nakache",'2011-01-01' , 19490688, 'francais' ), 
-("Tarzan" , 2, "Kevin Lima", '1999-01-01', 7859791, ''); 
+INSERT INTO score(pseudo,  score, datescore)
+VALUES('simondad', 20, '2021/12/12'),
+('simonmom', 15, '2021/12/12'), 
+('simonson', 10, '2021/12/12'), 
+('simonsister', 5, '2021/12/12'), 
+('simonbaby', 1, '2021/12/12');
 
 
-system cls; 
--- select * from .......;
+select * from score;
 
